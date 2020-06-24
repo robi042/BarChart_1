d8d2cecf3200cb7654253d936c3ad85779b1f019
Hasnain Ahmad Tanim <hatanim987@users.noreply.github.com>
2020-06-24 13:08:18 +0600

    Working version

--- app/src/main/java/com/example/barchart_1/MainActivity.java
+++ app/src/main/java/com/example/barchart_1/MainActivity.java
@@ -18,56 +18,48 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    BarChart barChart;
-    ArrayList<BarEntry> barEntryArrayList;
-    ArrayList<String> levelsNames;
-
-    ArrayList<monthSalesData> monthSalesDataArrayList = new ArrayList<>();
+    ArrayList<BarEntry> arrayList;
+    ArrayList<String> agentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        barChart = findViewById(R.id.barChart);
 
-        barEntryArrayList = new ArrayList<>();
-        levelsNames = new ArrayList<>();
-        fillMonthSales();
-        for(int i = 0; i<monthSalesDataArrayList.size(); i++){
-            String month = monthSalesDataArrayList.get(i).getMonth();
-            int sales = monthSalesDataArrayList.get(i).getSales();
-            barEntryArrayList.add(new BarEntry(i, sales));
-            levelsNames.add(month);
-        }
-        BarDataSet barDataSet = new BarDataSet(barEntryArrayList, "Monthly Sales");
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        Description description = new Description();
-        description.setText("Months");
-        barChart.setDescription(description);
-        BarData barData = new BarData();
-        barChart.setData(barData);
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(levelsNames));
-        xAxis.setPosition(XAxis.XAxisPosition.TOP);
+        BarChart doneRankChart= findViewById(R.id.barChart);
+        arrayList = new ArrayList();
+        agentName = new ArrayList<>();
+
+        arrayList.add(new BarEntry(1,100));
+        arrayList.add(new BarEntry(2,200));
+        arrayList.add(new BarEntry(3,300));
+        arrayList.add(new BarEntry(4,400));
+        arrayList.add(new BarEntry(5,500));
+
+        agentName.add("");
+        agentName.add("Sumon");
+        agentName.add("Raju");
+        agentName.add("Jahangir");
+        agentName.add("Hashem");
+        agentName.add("Pervej");
+        agentName.add("");
+
+        BarDataSet bardataset = new BarDataSet(arrayList, "Leaderboard");
+        doneRankChart.animateY(2000);
+        BarData data = new BarData(bardataset);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        doneRankChart.setData(data);
+
+        XAxis xAxis=doneRankChart.getXAxis();
+        xAxis.setLabelCount(agentName.size());
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(agentName));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+//        doneRankChart.getXAxis().setValueFormatter(agentName);
+        doneRankChart.getXAxis().setAxisMinValue(0f);
         xAxis.setDrawGridLines(false);
         xAxis.setDrawAxisLine(false);
-        xAxis.setGranularity(1f);
-        xAxis.setLabelCount(levelsNames.size());
         xAxis.setLabelRotationAngle(270);
-        barChart.animateY(2000);
-        barChart.invalidate();
-
-
+        xAxis.setGranularity(1f);
+        doneRankChart.invalidate();
     }
-   private void fillMonthSales(){
-        monthSalesDataArrayList.clear();
-        monthSalesDataArrayList.add(new monthSalesData("January, 2020", 225000));
-       monthSalesDataArrayList.add(new monthSalesData("February, 2020", 215000));
-       monthSalesDataArrayList.add(new monthSalesData("March, 2020", 185000));
-       monthSalesDataArrayList.add(new monthSalesData("April, 2020", 375000));
-       monthSalesDataArrayList.add(new monthSalesData("may, 2020", 140000));
-       monthSalesDataArrayList.add(new monthSalesData("June, 2020", 260000));
-       monthSalesDataArrayList.add(new monthSalesData("July, 2020", 125000));
-       monthSalesDataArrayList.add(new monthSalesData("August, 2020", 295000));
-   }
 }
